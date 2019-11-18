5660548f4f99afce67948fa0ff7ad5b7a0a8871b
and1kl <email.andreasklaiber@gmail.com>
2019-11-18 20:29:37 +0100

    Eureka eingebaut

--- Core_Product/src/main/java/de/hska/iwi/vslab/Core_Product/CoreProductApplication.java
+++ Core_Product/src/main/java/de/hska/iwi/vslab/Core_Product/CoreProductApplication.java
@@ -7,9 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CoreProductApplication {
 	private static final Logger log = LoggerFactory.getLogger(CoreProductApplication.class);
 
