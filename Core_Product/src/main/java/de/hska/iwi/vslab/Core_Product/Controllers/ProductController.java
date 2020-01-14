c6cac85cfcff11992231b3a17becfe1c7fbdf857
Marls2910 <marleen.eisenacht@gmx.de>
2020-01-14 22:06:29 +0100

    add annotation circuitBreaker

--- Core_Product/src/main/java/de/hska/iwi/vslab/Core_Product/Controllers/ProductController.java
+++ Core_Product/src/main/java/de/hska/iwi/vslab/Core_Product/Controllers/ProductController.java
@@ -7,6 +7,7 @@ import de.hska.iwi.vslab.Core_Product.Services.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@EnableCircuitBreaker
 public class ProductController {
 
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
